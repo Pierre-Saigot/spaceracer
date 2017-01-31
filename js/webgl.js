1755840b185a43fa5f5d6e5f08bff90f9d06d3f1
“Pierre-SAIGOT” <psaigot@gmail.com>
2017-01-31 11:16:12 +0100

    creat canvas

--- js/webgl.js
+++ js/webgl.js
@@ -7,7 +7,7 @@
 
 // Appel des Function Init & Animate
     init();
-    // animate();
+    animate();
  
 // Création de la fonction Init
 function init() {
@@ -19,7 +19,6 @@ function init() {
     canvas.width = 400;
     canvas.height = $(window).height();
     canvas.style.zIndex = 8;
-    canvas.style.background = "#1B1B1B";
 
 
 
@@ -28,48 +27,35 @@ function init() {
 
     // Création de la Caméra
     camera         = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 2000);
-    // camera.position.z     = 1700;
 
     // Création de la Scène
     scene         = new THREE.Scene();
-    // scene.fog     = new THREE.FogExp2( 0x000000, 0.0003 );
-
-    // Création des Objets
-    // geometry     = new THREE.Geometry();
-    //  textureLoader     = new THREE.TextureLoader();
-
-    // Importation des Sprites
-    
-    // sprite1     = ;
-    // sprite2     = ;
-    // sprite3    = ;
-
 
     // Création du Render
-    // renderer     = new THREE.WebGLRenderer();
-    // renderer.setPixelRatio( window.devicePixelRatio );
-    // renderer.setSize( window.innerWidth, window.innerHeight );
-    // container.appendChild( renderer.domElement );
-    // window.addEventListener( 'resize', onWindowResize, false );
+    renderer     = new THREE.WebGLRenderer();
+    var geometry = new THREE.CubeGeometry(1,1,1)
+    var material = new THREE.MeshNormalMaterial()
+    var mesh     = new THREE.Mesh(geometry, material)
+    scene.add(mesh)
 }
 
 // Fonction onResize du navigateur
-// function onWindowResize() {
-//     windowHalfX     = window.innerWidth / 2;
-//     windowHalfY     = window.innerHeight / 2;
+function onWindowResize() {
+    windowHalfX     = window.innerWidth / 2;
+    windowHalfY     = window.innerHeight / 2;
 
-//     camera.aspect     = window.innerWidth / window.innerHeight;
-//     camera.updateProjectionMatrix();
+    camera.aspect     = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
 
-//     renderer.setSize( window.innerWidth, window.innerHeight );
-// }
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
 // Fonction Animate
-// function animate() {
-//     requestAnimationFrame(animate);
-//     render();
-// }
+function animate() {
+    requestAnimationFrame(animate);
+    render();
+}
 
 // // Fonction Render
-// function render() {
-//     renderer.render( scene, camera );
-// }
+function render() {
+    renderer.render( scene, camera );
+}
